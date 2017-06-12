b67d2c156be51e6a22de519be7c43ea270a888f4
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-06-12 05:38:53 -0400

    Implement messages POST	method

--- src/main/java/com/intellimed/messenger/resources/MessageResource.java
+++ src/main/java/com/intellimed/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package com.intellimed.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -35,6 +37,13 @@ public class MessageResource {
 	 @Produces(MediaType.APPLICATION_JSON)
 	 public Message getMessages(@PathParam("messageId") long messageId){
 		 return messageService.getMessage(messageId);
+	 }
+	 
+	 @POST
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 @Produces(MediaType.APPLICATION_JSON)
+	 public Message addMessages(Message message){
+		 return messageService.addMessage(message);
 	 }
 	
 }
